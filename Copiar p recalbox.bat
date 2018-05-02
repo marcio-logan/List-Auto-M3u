78707ddd7a30f389a25a51193de090facd4ac3fc
Marcio <marcio.logan@gmail.com>
2018-05-02 20:41:10 -0300

    Atualização Servidor
    Atualização Servidores e novos Filmes

---
+++ Copiar p recalbox.bat
@@ -0,0 +1,7 @@
+rd /s /q "\\recalbox\share\kodi\List Auto M3u\Filmes"
+rd /s /q "\\recalbox\share\kodi\List Auto M3u\Series"
+rd /s /q "\\recalbox\share\kodi\List Auto M3u\Desenhos"
+
+xcopy Filmes "\\recalbox\share\kodi\List Auto M3u\Filmes\"
+xcopy Series "\\recalbox\share\kodi\List Auto M3u\Series\"
+xcopy Desenhos "\\recalbox\share\kodi\List Auto M3u\Desenhos\"
